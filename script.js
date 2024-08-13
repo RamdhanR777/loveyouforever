5c5cf6ab8c11dc3bdbbce148a9d19be936f81f39
RamdhanR777 <aadanaldo@gmail.com>
2024-08-13 07:48:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const revealButton = document.getElementById('revealButton');
+    const hiddenMessage = document.getElementById('hiddenMessage');
+
+    revealButton.addEventListener('click', () => {
+        hiddenMessage.style.display = 'block';
+        revealButton.style.display = 'none';
+    });
+});
